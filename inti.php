f0408412e65ccd267bf1031563153bc703f16722
asiyahhanifah <asiyahhanifah@gmail.com>
2022-04-18 22:55:35 +0700

    sql

--- inti.php
+++ inti.php
@@ -1,42 +1,50 @@
 
-<?php include "dbinfo.inc.php"; ?>
+
+<?php include "../inc/dbinfo.inc"; ?>
 <html>
 <body>
 <h1>Sample page</h1>
 <?php
 
+
   /* Connect to MySQL and select the database. */
   $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
+
   if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
+
   $database = mysqli_select_db($connection, DB_DATABASE);
+
 
   /* Ensure that the EMPLOYEES table exists. */
   VerifyEmployeesTable($connection, DB_DATABASE);
 
-  /* If input fields are populated, add a row to the EMPLOYEES table. */
-  $book_name = htmlentities($_POST['NAME']);
-  $employee_address = htmlentities($_POST['AUTHOR']);
 
-  if (strlen($book_name) || strlen($employee_address)) {
-    AddEmployee($connection, $book_name, $employee_address);
+  /* If input fields are populated, add a row to the EMPLOYEES table. */
+  $employee_name = htmlentities($_POST['NAME']);
+  $employee_address = htmlentities($_POST['ADDRESS']);
+
+
+  if (strlen($employee_name) || strlen($employee_address)) {
+    AddEmployee($connection, $employee_name, $employee_address);
   }
 ?>
+
 
 <!-- Input form -->
 <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
   <table border="0">
     <tr>
       <td>NAME</td>
-      <td>AUTHOR</td>
+      <td>ADDRESS</td>
     </tr>
     <tr>
       <td>
         <input type="text" name="NAME" maxlength="45" size="30" />
       </td>
       <td>
-        <input type="text" name="AUTHOR" maxlength="90" size="60" />
+        <input type="text" name="ADDRESS" maxlength="90" size="60" />
       </td>
       <td>
         <input type="submit" value="Add Data" />
@@ -45,17 +53,21 @@
   </table>
 </form>
 
+
 <!-- Display table data. -->
 <table border="1" cellpadding="2" cellspacing="2">
   <tr>
     <td>ID</td>
     <td>NAME</td>
-    <td>AUTHOR</td>
+    <td>ADDRESS</td>
   </tr>
+
 
 <?php
 
+
 $result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
+
 
 while($query_data = mysqli_fetch_row($result)) {
   echo "<tr>";
@@ -66,31 +78,41 @@ while($query_data = mysqli_fetch_row($result)) {
 }
 ?>
 
+
 </table>
+
 
 <!-- Clean up. -->
 <?php
 
+
   mysqli_free_result($result);
   mysqli_close($connection);
 
+
 ?>
+
 
 </body>
 </html>
 
 
+
 <?php
+
 
 /* Add an employee to the table. */
 function AddEmployee($connection, $name, $address) {
    $n = mysqli_real_escape_string($connection, $name);
    $a = mysqli_real_escape_string($connection, $address);
 
-   $query = "INSERT INTO EMPLOYEES (NAME, AUTHOR) VALUES ('$n', '$a');";
+
+   $query = "INSERT INTO EMPLOYEES (NAME, ADDRESS) VALUES ('$n', '$a');";
+
 
    if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
 }
+
 
 /* Check whether the table exists and, if not, create it. */
 function VerifyEmployeesTable($connection, $dbName) {
@@ -99,22 +121,27 @@ function VerifyEmployeesTable($connection, $dbName) {
      $query = "CREATE TABLE EMPLOYEES (
          ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
          NAME VARCHAR(45),
-         AUTHOR VARCHAR(90)
+         ADDRESS VARCHAR(90)
        )";
+
 
      if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
   }
 }
+
 
 /* Check for the existence of a table. */
 function TableExists($tableName, $connection, $dbName) {
   $t = mysqli_real_escape_string($connection, $tableName);
   $d = mysqli_real_escape_string($connection, $dbName);
 
+
   $checktable = mysqli_query($connection,
       "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
 
+
   if(mysqli_num_rows($checktable) > 0) return true;
+
 
   return false;
 }
---
+++ php.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_SERVER', 'database-1.cqnlmpqxohf8.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', '12345678');
+define('DB_NAME', 'sample');
+
+?>
